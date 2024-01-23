8f19a8dd6694c914639fd85a52d56a5cce43b640
evandalmeida <edalmei1@gmail.com>
2024-01-22 22:25:02 -0500

    add image to typing sequence

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import BELogo from "../assets/BM-hi.png"
+import './CSS/home.css'
 
 export default function Home() {
-    const fullText =  "I'm Evan D'Almeida ッ";
+    const fullText = "I'm Evan D'Almeida";
     const [displayText, setDisplayText] = useState('');
     const [isDeleting, setIsDeleting] = useState(false);
     const [speed, setSpeed] = useState(100);
@@ -10,14 +12,14 @@ export default function Home() {
     useEffect(() => {
         let timer;
 
-        // TYPING SEQEUNCE
+        // Typing effect
         if (!isDeleting && displayText.length < fullText.length) {
             timer = setTimeout(() => {
                 setDisplayText(fullText.slice(0, displayText.length + 1));
                 setSpeed(100);
             }, speed);
         } 
-        // DELETING SEQEUNCE
+        // Deleting effect
         else if (isDeleting) {
             timer = setTimeout(() => {
                 setDisplayText(fullText.slice(0, displayText.length - 1));
@@ -25,12 +27,12 @@ export default function Home() {
             }, speed);
         }
 
-        // SWTICH BETWEEN TYPING AND DELETING
+        // Switch between typing and deleting
         if (!isDeleting && displayText === fullText) {
             timer = setTimeout(() => {
                 setIsDeleting(true);
                 setSpeed(100);
-            }, 2000); 
+            }, 2000); // Wait a bit before starting to delete
         } else if (isDeleting && displayText === '') {
             setIsDeleting(false);
             setLoop(loop + 1);
@@ -42,7 +44,7 @@ export default function Home() {
 
     return (
         <div id='home-div'>
-            <h1>"Hello! {displayText}</h1>
+            <h1>Hello! {displayText}{displayText === fullText && <img className='BE-logo' src={BELogo} alt="ッ" />}</h1>
             <h2>I'm a software engineer with a knack for full-stack development and a special love for Python. My experience blends robust technical expertise with an innovative approach to tackling complex challenges.</h2>
         </div>
     );
