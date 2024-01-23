f68bc03f6a2baca6e86accc79495083010e6dc8b
evandalmeida <edalmei1@gmail.com>
2024-01-22 22:25:35 -0500

    removed logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,13 +6,6 @@ import './CSS/navBar.css'
 function NavBar() {
     return (
         <nav id='nav-bar'>
-                <Link to='/'>
-                    <div id='logo'>
-                        <h1 className='name'> EVAN</h1>
-                        <img className='BMLogo'src={headerImg} alt="Header Img"/>
-                    </div>
-                </Link>
-
             <Link to='/'> Home </Link>
             <Link to='/projects'> Projects </Link>
             <Link to='/about-me'> About Me </Link>
