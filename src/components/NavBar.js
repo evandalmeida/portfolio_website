de33d1a023753a46047307c1112bb228d576738a
evandalmeida <edalmei1@gmail.com>
2024-01-24 12:22:17 -0500

    navbar prototype

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,22 @@
 import React from  'react';
 import {Link} from 'react-router-dom'
+import BELogo from "../assets/BE-hi.png"
 import './CSS/navBar.css'
 
 function NavBar() {
     return (
+        <div id='main-navigation'>
+            <div id='logo'>
+                <Link className='name-logo' to='/'>EVAN <br/> D'ALMEIDA</Link>
+                <img className='BE-logo' src={BELogo} alt="ッ"/> 
+            </div>
         <nav id='nav-bar'>
-            <a href='/#home-div'> Home </a>
-            <a href='/#projects'> Projects </a>
-            <Link to='/about-me'> About </Link>
-            <Link to='/resume'> Resume </Link>
+            <Link to='/'> HOME </Link>
+            <a href='/#projects'> PROJECTS </a>
+            <Link to='/about-me'> ABOUT </Link>
+            <Link to='/resume'> RESUME </Link>
         </nav>
+        </div>
     )
 }
 
