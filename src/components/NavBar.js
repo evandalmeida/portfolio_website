a36f0d97053c2737a3704898fb4a136fa83fac72
evandalmeida <edalmei1@gmail.com>
2024-01-28 12:22:49 -0500

    navbar adj

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
+import React from  'react';
 import {Link} from 'react-router-dom'
-
-import './CSS/navBar.css'
-
 import BELogo from "../assets/ME/BE-hi.png"
+import './CSS/navBar.css'
 
 
 
 export default function NavBar({scrollToTop}) {
-    
-    const [navActive, setNavActive] = useState(false);
 
     const scrollToBottom = () => {
         window.scrollTo({
@@ -18,36 +14,19 @@ export default function NavBar({scrollToTop}) {
         });
     };
 
-
-    const toggleNav = () => {
-        setNavActive(!navActive);
-    };
-
     return (
         <div id='main-navigation'>
-
             <div id='logo'>
-                <a href='/#root' className='name-logo' onClick={scrollToTop}>
-                    EVAN <br/> D'ALMEIDA
-                </a>
-                <img className='BE-logo' src={BELogo} alt="Logo" /> 
+                <a href='/#root'className='name-logo' onClick={scrollToTop}>EVAN <br/> D'ALMEIDA</a>
+                <img className='BE-logo' src={BELogo} alt="ッ"/> 
             </div>
-
-
-
-
-            <div className="hamburger-menu" onClick={toggleNav}>
-                <div> OPEN </div>
-            </div>
-            <div id='nav-nav-bar' className={navActive ? 'nav-active' : ''}>
-                <nav>
-                    <a href='/#root' onClick={scrollToTop}>HOME</a>
-                    <Link to='/projects' onClick={scrollToTop}>PROJECTS</Link>
-                    <Link to='/about-me' onClick={scrollToTop}>ABOUT</Link>
-                    <Link to='/resume' onClick={scrollToTop}>RESUME</Link>
-                    <button onClick={scrollToBottom}>CONTACT ME</button>
-                </nav>
-            </div>
+            <nav id='nav-bar'>
+                <a href='/#root' onClick={scrollToTop} > HOME </a>
+                <Link to='/projects' onClick={scrollToTop}> PROJECTS </Link>
+                <Link to='/about-me' onClick={scrollToTop}> ABOUT </Link>
+                <Link to='/resume' onClick={scrollToTop}> RESUME </Link>
+                <button onClick={scrollToBottom}> CONTACT ME </button>
+            </nav>
         </div>
     );
 };
